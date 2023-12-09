f94c4f7bd787fa19e2b2bb8eb217d09cedcec385
Nguyen Dang Khoa <wentallout@gmail.com>
2023-12-09 11:43:51 +0700

    add some examples tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,22 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: {
+					100: '#fde6e9',
+					200: '#fbd0d9',
+					300: '#f7aab9',
+					400: '#f27a93',
+					500: '#e63f66',
+					600: '#d42a5b',
+					700: '#b21e4b',
+					800: '#951c45',
+					900: '#801b40',
+					950: '#470a1f'
+				}
+			}
+		}
 	},
 
 	plugins: [forms, typography]
